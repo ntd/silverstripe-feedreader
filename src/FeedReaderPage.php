5309d7e962a896a5afe956f37497ab77b80d9065
Nicola Fontana <ntd@entidi.it>
2017-12-19 14:31:43 +0100

    Update FeedReaderPage

--- src/FeedReaderPage.php
+++ src/FeedReaderPage.php
@@ -3,6 +3,7 @@
 namespace eNTiDi\FeedReader;
 
 use Page;
+use eNTiDi\FeedReader\FeedReaderService;
 
 /**
  * Defines the FeedReaderPage page type.
@@ -26,34 +27,30 @@ class FeedReaderPage extends Page
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
-
-        $fields->addFieldsToTab('Root.Main', [
+        $fields->addFieldsToTab('Root.Feed', [
             TextField::create('FeedUrl', _t('FeedReader.FEED_URL')),
             NumericField::create('SummaryLen', _t('FeedReader.SUMMARY_LEN'))
                 ->setDescription(_t('FeedReader.SUMMARY_LEN_COMMENT')),
             NumericField::create('Expiration', _t('FeedReader.EXPIRATION'))
                 ->setDescription(_t('FeedReader.EXPIRATION_COMMENT'))
-        ], 'Content');
-
+        ]);
         return $fields;
     }
 
-    private $_service;
+    private $service;
 
-    private function _getService()
+    private function getService()
     {
-        if (is_null($this->_service)) {
-            $this->_service = new FeedReaderService($this->FeedUrl, $this->Expiration);
-            $this->_service->setSummaryLen($this->SummaryLen);
+        if (! $this->service) {
+            $this->service = new FeedReaderService($this->FeedUrl, $this->Expiration);
+            $this->service->setSummaryLen($this->SummaryLen);
         }
-
-        return $this->_service;
+        return $this->service;
     }
 
     public function Items($count = null)
     {
-        $service = $this->_getService();
-        $items = $service->getItems();
+        $items = $this->getService()->getItems();
 
         // When $count is null, limit() will use array_slice(..., 0, null)
         // internally, meaning a clone of $items will be returned
